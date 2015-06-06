748e3f5dc7f0622178ab06c0d29b99213b1930f7
Robert Farber <elminero@cox.net>
2015-06-05 18:01:23 -0700

    Fixed logout link in header

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,15 @@
     <ul  class="nav">
         <li><a href="listcontacts.php">List of All Contacts</a></li>
         <li><a href="newcontact.php">New Contact</a></li>
-        <li><a href="controllers/loginController.php?action=logout">Log Out</a></li>
+        <li><a href="controllers/LoginController.php?action=logout">Log Out</a></li>
     </ul>
 </div><!-- end .header -->
+
+
+
+<!--
+http://localhost/contact_v1/controllers/loginController.php?action=logout
+
+http://localhost/contact_v1/controllers/LoginController.php
+
+-->
