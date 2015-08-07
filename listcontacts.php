ff512d04874d05efd4a9a3c480e2d86fe981a235
Robert Farber <elminero@cox.net>
2015-08-06 20:06:50 -0700

    Clean up on listcontacts.php

--- listcontacts.php
+++ listcontacts.php
@@ -4,7 +4,6 @@
 // require("models/Contact.php");
 
 require("models/Person.php");
-
 require("controllers/LoginController.php");
 
 $login = new LoginController();
@@ -14,17 +13,11 @@ if ($login->login == 0) {
     header("Location: login.php");
 }
 
-
 $contact = new PersonPDO();
-
-
 $stmt = $contact->getAllPerson();
 
-
-
-
-
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" >
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -45,21 +38,17 @@ $stmt = $contact->getAllPerson();
             <b>List</b>
         </div>
         <div style="clear: both"></div>
-
-    <table>
-        <?php while($row = $stmt->fetch(PDO::FETCH_OBJ)): ?>
-            <?php $nameLastFirstMiddle = $row->first_name . " " . $row->middle_name  . " " . $row->last_name ; ?>
-            <tr>
-                <td><a href="profile.php?=<?php echo $row->id; ?>"><?php echo $nameLastFirstMiddle; ?></a>        </td>
-            </tr>
-        <?php endwhile ?>
-    </table>
-
+        <table>
+            <?php while($row = $stmt->fetch(PDO::FETCH_OBJ)): ?>
+                <?php $nameLastFirstMiddle = $row->first_name . " " . $row->middle_name  . " " . $row->last_name ; ?>
+                <tr>
+                    <td><a href="profile.php?=<?php echo $row->id; ?>"><?php echo $nameLastFirstMiddle; ?></a></td>
+                </tr>
+            <?php endwhile ?>
+        </table>
     </div><!-- end .content -->
 
-    <?php
-    include("includes/footer.php");
-    ?>
+    <?php include("includes/footer.php"); ?>
 
 </div><!-- end .container -->
 </body>
