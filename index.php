e56e3bd57a464f10b938a0737328e56754a9c5dd
Robert Farber <elminero@cox.net>
2016-01-27 01:30:48 -0800

    clean up white space on index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+
     <link rel="stylesheet" type="text/css" href="css/main.css"/>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Online Contacts</title>
@@ -14,19 +20,14 @@
                 <li><a href="login.php">Login</a></li>
             </ul>
         </div><!-- end .header -->
-
         <img src="css/dark_road_forest850c.jpg" />
-<!--
-        <div class="content">
-            <div style="margin-bottom: 9px">
-            </div>
-            <div style="clear: both"></div>
-        </div><!-- end .content -->
-
-    
     </div><!-- end .container -->
     <?php
     include("includes/footer.php");
     ?>
+
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
