e83ec229e018e75d6a4c2149367bbdd31fa6a339
Robert Farber <elminero@cox.net>
2016-01-27 13:10:54 -0800

    index.php set up as bootstrap test

--- index.php
+++ index.php
@@ -6,25 +6,75 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
-
-    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    <!--<link rel="stylesheet" type="text/css" href="css/main.css"/>-->
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Online Contacts</title>
 </head>
 <body>
-<div class="container"><!-- start .container -->
-    <div class="header">
-        <div class="header"><!-- Start .header -->
+    <div class="container"><!-- start .container -->
+
+        <div class="row">
+            <div class="col-md-2">
+                <h1>Headline</h1>
+                <p>
+                    Harney County Sheriff David Ward on Wednesday blamed the occupiers of a federal wildlife refuge in
+                    Oregon for the death of an outspoken protester who authorities tried to arrest during a traffic stop
+                    tied to the ordeal.Authorities have not identified the person who was shot and killed during
+                    Tuesday's stop, but others have identified him as LaVoy Finicum. Protest leader Ammon Bundy was
+                    arrested during the same stop. "Multiple agencies put a lot of work into this to put the best
+                    tactical plan that they could to arrest them peacefully," Ward told reporters. "(The death) didn't
+                    have to happen. We all make choices in life. Sometimes our choices go
+                </p>
+            </div>
+
+            <div class="col-md-2">
+                <h1>Headline</h1>
+                <p>
+                    Harney County Sheriff David Ward on Wednesday blamed the occupiers of a federal wildlife refuge in
+                    Oregon for the death of an outspoken protester who authorities tried to arrest during a traffic stop
+                    tied to the ordeal.Authorities have not identified the person who was shot and killed during
+                    Tuesday's stop, but others have identified him as LaVoy Finicum. Protest leader Ammon Bundy was
+                    arrested during the same stop. "Multiple agencies put a lot of work into this to put the best
+                    tactical plan that they could to arrest them peacefully," Ward told reporters. "(The death) didn't
+                    have to happen. We all make choices in life. Sometimes our choices go
+                </p>
+            </div>
+
+            <div class="col-md-2">
+                <h1>Headline</h1>
+                <p>
+                    Harney County Sheriff David Ward on Wednesday blamed the occupiers of a federal wildlife refuge in
+                    Oregon for the death of an outspoken protester who authorities tried to arrest during a traffic stop
+                    tied to the ordeal.Authorities have not identified the person who was shot and killed during
+                    Tuesday's stop, but others have identified him as LaVoy Finicum. Protest leader Ammon Bundy was
+                    arrested during the same stop. "Multiple agencies put a lot of work into this to put the best
+                    tactical plan that they could to arrest them peacefully," Ward told reporters. "(The death) didn't
+                    have to happen. We all make choices in life. Sometimes our choices go
+                </p>
+            </div>
+
+
+
+        </div>
+
+
+
+
+        <!--
+        <div class="header">
             <ul  class="nav">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="login.php">Login</a></li>
             </ul>
-        </div><!-- end .header -->
-        <img src="css/dark_road_forest850c.jpg" />
+        </div>
+        <img src="css/dark_road_forest850c.jpg" />-->
+        <?php // include("includes/footer.php"); ?>
+
+
     </div><!-- end .container -->
-    <?php
-    include("includes/footer.php");
-    ?>
+
+
+
 
     <script src="js/jquery-1.12.0.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
